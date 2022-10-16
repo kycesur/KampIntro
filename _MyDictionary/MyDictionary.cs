5af166ebfbac437a469077b129ad81c8650076d6
Kadir <Kadir@DESKTOP-98TS8Q8>
2022-10-16 22:48:03 +0300

    16.06.2022 Odev

---
+++ _MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _MyDictionary
+{
+    internal class MyDictionary<TKey,TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+        public MyDictionary()
+        {
+          _key = new TKey[0];
+            _value = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKeyArray = _key;
+            TValue[] tempValueArray= _value;
+            _key = new TKey[_key.Length + 1];
+            _value= new TValue[_value.Length+1];
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _key[i] = tempKeyArray[i];
+            }
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                _value[i] = tempValueArray[i];
+            }
+            _key[_key.Length-1] = key;
+            _value[_value.Length-1] = value;
+        }
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+    }
+}
---
+++ _MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(78, "Karabük");
+            myDictionary.Add(74, "Bartın");
+           
+          
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
